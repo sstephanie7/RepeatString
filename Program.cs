d5546298d3fd69d8dd21fd838e266689518544d2
Stephanie Tahova <106015292+sstephanie7@users.noreply.github.com>
2022-06-16 17:29:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _07._Repeat_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int counter = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(RepeatString(input, counter));
+        }
+        static string RepeatString (string input, int counter)
+        {
+            string result = string.Empty;
+
+            for(int i = 0; i < counter; i++)
+            {
+                result = result + input;
+            }
+
+            return result; 
+        }
+    }
+}
